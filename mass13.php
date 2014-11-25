76a013209703d26026b55287e7115c0d353e2674
bobgromozeka <bobgromozeka@gmail.com>
2014-11-26 00:41:13 +0200

    Коммит был из непеределанного пхп (закинул верный)

--- mass13.php
+++ mass13.php
@@ -2,14 +2,20 @@
 var N;
 var numb;
 var blizh = 0;
+var min = 0;
 get(N);
 get(numb);
 var A = array();
 for (int i = 0; i < N; i++){
 	get(A[i]);
 }
-abs(A[0] - numb) = blizh;
+blizh = A[0];
+min = abs(A[0] - numb);
 for (int i = 1; i < N; i++){
-	if (abs(A[i] - numb) < blizh) blizh = A[i];
+	if (abs(A[i] - numb) < min){ 
+		blizh = A[i];
+		min = abs(A[i] - numb);
+	}
 }
 print("Ближайший элемент: " + blizh);
+?>
