be5652d100f2043f2af754f11a0016d94d15f06c
bobgromozeka <bobgromozeka@gmail.com>
2014-12-03 21:53:12 +0300

    Код для задания по аренде жилья

--- housing.php
+++ housing.php
@@ -1,1 +1,57 @@
 <?php
+abstract class Housing
+{
+	public square; //площадь
+	public quality; //Состояние жилья(слабо пригодное, идеальное и т.д.)
+	public number_of_floors; //количество этажей
+	public number_of_rooms; //количество комнат(для комнаты это поле = 1)
+	public price_per_sqr_meter; //цена за квадратный метр
+	public isAnimalsAllowed; //дозволены ли животные
+	public rent_term; //срок сдачи
+
+	abstract public function CountPrice(){ //подсчет цены дневной оплаты за жилье
+    
+	}
+
+	public function setRentTerm(term){  //установка срока сдачи жилья 
+		this.rent_term = term;
+		return ...
+	}
+
+	public function isBusy(rent_term){ //жилье считается занятым, если срок сдачи больше 0
+		if(this.rent_term > 0){
+		 return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+}
+
+class House extends Housing
+{
+	public yard_square; //плозадь двора
+	public price_per_yard_sqr_meter; //цена за квадратный метр двора
+	public yard_quality; //состояние двора
+
+	public function CountPrice(){ //подсчет цены дома(с учетом цены двора и жилого помещения)
+		return this.price_per_yard_sqr_meter*this.yard_square+this.square*this.price_per_sqr_meter;
+	}
+}
+
+class Apartment extends Housing
+{
+	public floor; //этаж квартиры
+	public function CountPrice(){ //подсчет цены квартиры
+		return this.square*this.price_per_sqr_meter;
+	}
+}
+
+class Room extends Housing
+{
+	public floor; //этаж квартиры в которой комната
+	public function CountPrice(){ //подсчет цены комнаты
+		return this.square*this.price_per_sqr_meter;
+	}
+}
