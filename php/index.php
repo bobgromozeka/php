57df249ad555e86ccdcb909ae54f30ed5b482567
bobgromozeka <bobgromozeka@gmail.com>
2015-04-26 18:54:41 +0300

    new index.php

--- php/index.php
+++ php/index.php
@@ -2,9 +2,90 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style>
+			body{
+				background-color: pink;
+				color:blue;
+			} 
+		 	
+			p{
+				margin-bottom: 25px;
+			}
+			label.myclass{
+				color:blue;
+			}
+
+			label#login{
+				margin-right:40px;
+			}
+			label#password{
+				margin-right:13px;
+			}
+			label#name{
+				margin-right:35px; 
+			}
+			label#email{
+				margin-right: 29px;
+			}
+			label#country{
+				margin-right: 21px;
+			}
+			label#ciferka{
+				margin-right: 85px;
+			}
+			a.bottom_link{
+				color:green;
+				font-family: Arial;
+			}
+			a.bottom_link:visited{
+				color:black;
+			}
+			a.lipsum{
+				color:aqua;
+			}
+			a.lipsum:visited{
+				color:darkred;
+			}
+			form #a1,#a2{
+				color:white;
+			}
+			#country{
+				width: 173px;
+				margin-left: 16px;
+			}
+			#country_label{
+				padding-right: 3px;
+			}
+			form{
+				background: lightpink;
+				width: 35%;
+				margin: 0 auto;
+				size: 100%;
+				padding-top: 10px;
+				padding-bottom: 4%;
+			}
+			
+		</style>
+		<script>
+		</script>
 </head>
-<body>
+<body onload="disab()">
 <?php
+function bget()
+{
+	echo "Массив GET: ";
+	print_r($_GET);
+}
+function bpost()
+{
+	echo "Массив POST: ";
+	print_r($_POST);
+}
+function bfiles()
+{
+	echo "Массив FILES: ";
+	print_r($_FILES);
+}
 echo "Вывод имени: <br />";
 $name = "name";
 $age = 17;
@@ -76,6 +157,57 @@ for ($i = 0; $i<=10;$i++)
 	if ($arr3[$i]<0) $arr3[$i]=0;
 }
 print_r($arr3);
+echo "<br /><br /><br /><br /><br />"; 
 ?>
+<div id="main_text">
+<form method="get" action="index.php" align="center">
+		<p><label>Форма для отправки методом GET</label></p>
+		<p><label class="myclass" id="login" for="login">login</label><input name="login" type="text"></p>
+		<p><label class="myclass" id="password" for="password">password</label><input name="password" type="password"></p>
+		<p><label class="myclass" id="name" for="name">Name</label><input name="name" type="text"></p>
+		<p><label class="myclass" id="email" for="email">E-mail</label><input name="email" type="email"></p>
+		<p><label id="country_label" for="country">Country</label><select id="country" name="country">
+		<option value="Ukraine">Украина</option>
+		<option value="Russia">Россия</option>
+		<option value="Belorussia">Белоруссия</option>1
+		</select></p>
+		<p><label id="ciferka" for="country">Выберите циферку</label><select name="ciferka" multiple size="3">
+		<option value="1">1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
+		<option value="5">5</option>
+		<option value="6">6</option>
+		</select></p>
+		<p><textarea rows=4 cols=25>О себе</textarea></p>
+		<p><input name="Terms" type="checkbox">Принимаю условия соглашения</p>
+		<p><label for="gender">Gender</label><input name="gender" type="radio" value="Male">Male
+		<input name="gender" type="radio" value="Female">Female</p>
+		<p><input type="submit" id="butt"></p>
+		<a id="a1" class="bottom_link" href="http://google.ru">Гугл зачем-то</a>
+		<a id="a2" class="bottom_link" href="http://ya.ru">Yandex зачем-то</a>
+		<a id="a3" class="bottom_link" href="http://youtube.ru">Youtube зачем-то</a>
+	</form>
+	</div>
+	<?php
+	echo "<br /><br /><br />";
+	bget();
+	echo "<br /><br /><br />";
+	 ?>
+	 <form method="post" action="index.php" align="center" enctype="multipart/form-data">
+	 <p><label>Форма для отправки методом POST и для загрузки файла</label></p>
+	 <p><label class="myclass">login</label><input name="login" type="text"></p>
+	 <p><label class="myclass">password</label><input name="password" type="password"></p>
+	 <p><input type="file" name="uploadfile">
+	 <input type="submit" value="Загрузить">
+	 <p><input type="submit" id="butt"></p>
+	 </form>
+	 <?php
+	echo "<br /><br /><br />";
+	bpost();
+	echo "<br /><br /><br />";
+	bfiles();
+	echo "<br /><br /><br />";
+	 ?>
 </body>
 </html>
